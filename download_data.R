f7ca049b69bb0fb7bed183bd0a61c8235f769d1b
Ryan Bansal <ryanbansal04@gmail.com>
2025-07-03 20:59:23 -0400

    Final

--- download_data.R
+++ download_data.R
@@ -3,3 +3,19 @@ library(dplyr)
 library(lubridate)
 library(ggplot2)
 
+station_id <- "727930-24233"
+rds_path <- "data/seattle_weather_2024.rds"  # save in a data/ folder
+
+if (!file.exists(rds_path)) {
+  seattle_weather <- importNOAA(
+    code = station_id,
+    year = 2024,
+    hourly = TRUE,
+    path = tempdir()
+  )
+  dir.create("data", showWarnings = FALSE)  # create folder if missing
+  saveRDS(seattle_weather, rds_path)
+  message("Data downloaded and saved.")
+} else {
+  message("Data already exists. Skipping download.")
+}
